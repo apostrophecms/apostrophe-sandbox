c17c4488bd9e5d2fab9bb6a4c18da1ba277e2eff
Kerry Gilbert <kerry@egdod-iv.punk>
2013-02-08 16:35:37 -0500

    Use

---
+++ data/local.js
@@ -0,0 +1,9 @@
+// Settings specific to this server. Change the URL
+// if you are deploying in production. Then copy to
+// data/local.js. That folder is shared by all 
+// deployments in our stagecoach recipe
+
+module.exports = {
+  uploadsUrl: 'http://localhost:3000/uploads'
+}
+
