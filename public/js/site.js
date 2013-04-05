1b69a891bec420035f4d27e3875d7456893d24d8
Tom Boutell <tom@punkave.com>
2013-04-04 20:18:42 -0400

    Compatible with latest published releases of modules, including the new refresh mechanism

--- public/js/site.js
+++ public/js/site.js
@@ -1,12 +1,13 @@
-// Replace the generic type objects from the server with versions that
-// offer custom page settings, etc. Make sure we wait for domready so
-// we go after the initialization of the pages module
+// Truly site-specific JS
 
 $(function() {
-  aposPages.replaceType('snippets', new AposSnippets());
-  AposSnippets.addWidgetType();
-  aposPages.replaceType('blog', new AposBlog());
-  AposBlog.addWidgetType();
-  aposPages.replaceType('map', new AposMap());
-  AposMap.addWidgetType();
+  // TODO: refactor these into the apostrophe module
+  $('.apos-accordion-title').click(function(event){
+    $(this).parent().find('.apos-accordion-items').toggleClass('open');
+  });
+
+  $('.apos-preview-toggle').click(function(event){
+    $('.apos-preview-toggle').toggleClass('previewing');
+    $('body').toggleClass('previewing');
+  });
 });
