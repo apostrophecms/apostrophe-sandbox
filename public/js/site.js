9aebed33452e025ad727f08d6bb51fcc3431f37d
Tom Boutell <tom@punkave.com>
2013-06-20 09:52:38 -0400

    * Reverted the decision to attempt the global js calls on every content refresh, this does not make sense, instead we trigger aposReady after the contentRefresh for any handlers that are interested in that situation. Ideally you just write event handlers bound on the body and filtering by your selector so this doesn't matter to you * New site.js file starts out empty as it ought to

--- public/js/site.js
+++ public/js/site.js
@@ -1,79 +1,41 @@
 // Truly site-specific JS
+//
+// YOU SHOULD USE .on ON THE BODY, to avoid problems recognizing events
+// on elements added later. Example:
+//
+// RIGHT
+//
+// // I work with content added later
+// $(function() {
+//   $('body').on('click', '.my-selector', function() { ... })
+// }
+//
+// WRONG
+//
+// // I won't work with content added dynamically!
+// $(function() {
+//   $('.my-selector').click(function() { ... })
+// });
+//
+// If this doesn't work for your needs, listen for the 'aposReady'
+// event on the body element, and update your event handlers on other
+// elements whenever your receive it, taking care NOT to double-bind.
+// Use jQuery's $.data to figure out if you've bound this element already.
+//
+// Example:
+//
+// $('body').on('aposReady', function() {
+//   $('.my-fancy-things').each(function() {
+//     var $thing = $(this);
+//     if (!$thing.data('aposLive')) {
+//       $thing.data('aposLive', true);
+//       // Do stuff with $thing that's so amazing you can't do it with
+//       // event handlers on 'body' filtered by a selector
+//     }
+//   });
+// });
 
 $(function() {
-  // TODO: refactor these into the apostrophe module
-  $('.apos-accordion-title').click(function(event){
-    $(this).parent().find('.apos-accordion-items').toggleClass('open');
-  });
-
-  $('.apos-preview-toggle').click(function(event){
-    $('.apos-preview-toggle').toggleClass('previewing');
-    $('body').toggleClass('previewing');
-  });
+  // Your code goes here
 });
-
-
-function MyBlog(optionsArg) {
-  var self = this;
-  var options = {
-    instance: 'blogPost',
-    name: 'blog'
-  };
-  $.extend(options, optionsArg);
-  AposSnippets.call(self, options);
-
-  function findExtraFields($el, data, callback) {
-    data.publicationDate = $el.find('[name="publication-date"]').val();
-    data.publicationTime = $el.find('[name="publication-time"]').val();
-
-    callback();
-  }
-
-  self.afterPopulatingEditor = function($el, snippet, callback) {
-    $el.find('[name="publication-date"]').val(snippet.publicationDate);
-    $el.find('[name="publication-time"]').val(snippet.publicationTime);
-
-    $(function() {
-      $el.find('[name="publication-date"]').datepicker({
-        defaultDate: "+0w",
-        changeMonth: true,
-        numberOfMonths: 1,
-        onClose: function( selectedDate ) {
-          console.log(selectedDate);
-          $el.find('[name="publication-date"]').datepicker( "option", "minDate", selectedDate );
-        }
-      });
-    });
-
-    callback();
-  };
-
-  self.addingToManager = function($el, $snippet, snippet) {
-    $snippet.find('[data-date]').text(snippet.publicationDate);
-    if (snippet.tags !== null) {
-      $snippet.find('[data-tags]').text(snippet.tags);
-    }
-  };
-
-  self.beforeInsert = function($el, data, callback) {
-    findExtraFields($el, data, callback);
-  };
-
-  self.beforeUpdate = function($el, data, callback) {
-    findExtraFields($el, data, callback);
-  };
-}
-
-MyBlog.addWidgetType = function(options) {
-  if (!options) {
-    options = {};
-  }
-  _.defaults(options, {
-    name: 'blog',
-    label: 'Blog Posts',
-    action: '/apos-blog',
-    defaultLimit: 5
-  });
-  AposSnippets.addWidgetType(options);
-};
 
