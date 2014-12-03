23799b847cf9da52e1ea3434d239f0c2c8f8058c
Stuart Romanek <stuart@punkave.com>
2014-12-03 16:48:56 -0500

    responsive/off-canvas

--- public/js/modules/off-canvas.js
+++ public/js/modules/off-canvas.js
@@ -1,8 +1,14 @@
 $(function(){
 	var $body = $('body');
 	var $trigger = $('.off-canvas-trigger');
+	var $close = $('.demo-nav-close');
 
 	$trigger.on('click', function() {
+		$body.toggleClass('show-off-canvas');
+		return false;
+	});
+
+	$close.on('click', function() {
 		$body.toggleClass('show-off-canvas');
 		return false;
 	});
