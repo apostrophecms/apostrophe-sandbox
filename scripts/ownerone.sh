2586913389548aaad75af0c450b3fbdc9ac09009
Tom Boutell <tom@punkave.com>
2014-12-05 16:52:29 -0500

    updated owners list

--- scripts/ownerone.sh
+++ scripts/ownerone.sh
@@ -6,10 +6,11 @@ npm owner add boutell $1
 npm owner add colpanik $1
 npm owner add jsumnersmith $1
 npm owner add alexgilbert $1
-npm owner add gsf $1
 npm owner add stuartromanek $1
 npm owner add livhaas $1
 npm owner add mcoppola $1
+npm owner add suhmantha1 $1
+npm owner add jimmyh $1
 # Have not created npm accounts
 # npm owner add geoffdimasi $1
 # npm owner add tquirino $1
