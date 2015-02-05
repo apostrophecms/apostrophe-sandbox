991e0eb521d65107b12c73036d45f5dc2efd52c5
Tom Boutell <tom@punkave.com>
2015-02-05 10:13:49 -0500

    refactored for readability

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var apos = require('apostrophe')({
+  shortName: 'sandbox',
+  hostName: 'sandbox',
   afterInit: function(callback) {
     console.log('apos.assets is:');
     console.log(apos.assets);
+    return setImmediate(callback);
   }
 });
