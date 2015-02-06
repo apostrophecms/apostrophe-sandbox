d288cd57875d74486d30123d1cc7f7b2e6effbfe
Tom Boutell <tom@punkave.com>
2015-02-06 14:54:13 -0500

    ignore updates

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+var _ = require('lodash');
 var apos = require('apostrophe')({
   shortName: 'sandbox',
   hostName: 'sandbox',
+  modules: {
+    'apostrophe-assets': {
+      minify: true
+    }
+  },
   afterInit: function(callback) {
-    console.log('apos.assets is:');
-    console.log(apos.assets);
     return setImmediate(callback);
   }
 });
