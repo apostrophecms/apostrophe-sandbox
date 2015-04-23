696ef02b49ba2f9e666583362da1c598717f5d1c
Tom Boutell <tom@punkave.com>
2015-04-23 11:17:07 -0400

    We can add a widget! It doesn't save yet, but that's something!

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var apos = require('apostrophe')({
     'link-widgets': {},
     'apostrophe-express': {
       middleware: [
+        // for now, always log the user in as admin.
+        // We'll remove this when the apostrophe-auth module
+        // is ready.
         function(req, res, next) {
           req.user = {
             _id: 'admin',
