e40dec26300d47f009b269ee668909ae253d6d33
Tom Boutell <tom@punkave.com>
2016-02-23 14:25:17 -0500

    ownerall.sh consults our-modules.source

--- scripts/ownerall.sh
+++ scripts/ownerall.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-for MODULE in $(find node_modules -depth 1 -type l)
+source scripts/our-modules.source
+for MODULE in "${modules[@]}"
+# for MODULE in $(find node_modules -depth 1 -type l)
 do
+  echo $MODULE
   (sh scripts/ownerone.sh `basename $MODULE`)
 done
 
--- scripts/ownerone.sh
+++ scripts/ownerone.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-npm owner add joshuatrii $1
-npm owner add benirose $1
-npm owner add kylestetz $1
 npm owner add boutell $1
 npm owner add colpanik $1
 npm owner add jsumnersmith $1
 npm owner add alexgilbert $1
 npm owner add stuartromanek $1
-npm owner add livhaas $1
 npm owner add mcoppola $1
 npm owner add suhmantha1 $1
 npm owner add jimmyh $1
 npm owner add kyjoya $1
-npm owner add dkh202 $1
+npm owner add austinstarin $1
