44355dfc1481897b9450cc355fe61ebe6302c07a
Tom Boutell <tom@punkave.com>
2017-08-17 20:21:54 -0400

    updated our silly "why don't we have an org" scripts

--- scripts/ownerone.sh
+++ scripts/ownerone.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-npm owner add boutell $1
-npm owner add colpanik $1
-npm owner add alexgilbert $1
-npm owner add stuartromanek $1
-npm owner add mcoppola $1
-npm owner add jimmyh $1
-npm owner add kyjoya $1
-npm owner add austinstarin $1
-npm owner add grdunn $1
+npm owner add boutell $1 &
+npm owner add colpanik $1 &
+npm owner add alexgilbert $1 &
+npm owner add stuartromanek $1 &
+npm owner add austinstarin $1 &
+npm owner add grdunn $1 &
+npm owner add alexbea $1 &
+npm owner add bgantick $1 &
+npm owner add arti5m $1 &
+npm owner add bobclewell $1 &
+wait
+
