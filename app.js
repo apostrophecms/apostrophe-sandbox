c758442bcb324f46ff83b7daa0b2ee7fe24e2def
Tom Boutell <tom@punkave.com>
2017-10-23 14:02:11 -0400

    submit widgets working example

--- app.js
+++ app.js
@@ -30,6 +30,39 @@ var apos = require('apostrophe')({
     // that should let EVERYBODY be INSTANTLY loggged in AS ADMIN!
     'demo-autologin': {},
 
+    // For example: we're using the "apostrophe-events" module
+    // and we'll want submissions of events. "apostrophe-events"
+    // extends apostrophe-pieces
+    'apostrophe-events': {
+      // Let's add an attachment field so the user can upload an image
+      addFields: [
+        {
+          name: 'image',
+          type: 'attachment',
+          group: 'images',
+          required: true
+        }
+      ]
+    },
+    // ** The name you give this module is significant. **
+    // It should begin with the name of the pieces module you want
+    // to add the submissions feature to, and end with -submit-widgets
+    'apostrophe-events-submit-widgets': {
+      // Your module extends this one, and adds capabilities
+      // to your pieces module
+      extend: 'apostrophe-pieces-submit-widgets',
+      // Always spell out the schema field names the user is allowed to edit.
+      // You almost certainly don't want them to have control
+      // of the "published" field, for instance
+      fields: [ 'title', 'body', 'image', 'startDate', 'endDate' ]
+    },
+    // Optional: if you want to let the public attach files.
+    // See the `addFields` call above
+    'apostrophe-permissions': {
+      construct: function(self, options) {
+        self.addPublic('edit-attachment');
+      }
+    }
   }
 
 });
